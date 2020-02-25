13e9dc82b5eb2decfea8b6e53bd5f33881b358cd
Tombaysbot <47663424+Tombaysbot@users.noreply.github.com>
2020-02-25 17:26:29 +0300

    Add files via upload

--- gen.py
+++ gen.py
@@ -1,34 +1,16 @@
 from pdf417gen import encode, render_image
-import sys
+import sys, argparse
 
-if len(sys.argv) == 1:
-	print ("use '-h' to help")
-else:
-
-	if len(sys.argv) > 3:
-		print ("too many arguments")
-	else:
-
-		if str(sys.argv[1]) == "-h":
-			print ("""Script created by Tombays
-arguments:
--h  help
-positional arguments:
-[TEXT TO ENCODE]
-[OUTPUT FILE NAME.jpg/.png]
-
-example:
-
-python3 gen.py "Hello World" "barcode.jpg"
-""")
-		else:
-
+parser = argparse.ArgumentParser(description='this script can make pdf 417 barcodes')
+parser.add_argument('TEXT', type=str, help='defines text that you going to encode')
+parser.add_argument('-o','--output', type=str, required=True, help='defines output path + file. e.g: -o Dir/pic.jpg')
+args=parser.parse_args()
 # Some data to encode
-			text = str(sys.argv[1])
+text = args.TEXT
 
 # Convert to code words
-			codes = encode(text)
+codes = encode(text)
 
 # Generate barcode as image
-			image = render_image(codes)  # Pillow Image object
-			image.save(str(sys.argv[2]))
+image = render_image(codes)  # Pillow Image object
+image.save(args.output)
